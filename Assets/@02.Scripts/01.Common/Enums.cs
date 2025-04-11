91adb88ecb6a7a2cbc48c35b342891d4773a34a4
pianoop <kphjjh723@gmail.com>
2025-04-12 01:06:26 +0900

    chore: Initial project setup

---
+++ Assets/@02.Scripts/01.Common/Enums.cs
@@ -0,0 +1,25 @@
+
+
+namespace PlayerEnums
+{
+    
+    
+    
+    
+    //Example
+    public enum WarriorSkillType
+    {
+        ShieldBash,
+        RicochetShield,
+        BladeWave,
+        PhantomBlades
+    }
+}
+
+namespace ItemEnums
+{
+    public enum ItemType
+    {
+        
+    }
+}
