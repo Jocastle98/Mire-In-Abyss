d26e55fddd655b79e8cb6846df8c481a6e7bb166
Yoon Gun <dbsrjs0514@naver.com>
2025-04-24 00:31:05 +0900

    Revert "Merge branch 'develop' into YoonGun"
    This reverts commit e6f5fe6d91dabbf17c20d22e2b98531edfd55789, reversing
    changes made to 6b9b2cb8ac6e4aaec44966afd058fc41a26a8f4c.

--- Assets/@02.Scripts/01.Common/CameraController.cs
+++ Assets/@02.Scripts/01.Common/CameraController.cs
@@ -8,19 +8,14 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private float mRotationSensitivity = 1.0f;
     [SerializeField] private GameObject mCinemachineCameraTarget;
+    [SerializeField] private float mTopClamp = 70.0f;
+    [SerializeField] private float mBottomClamp = -30.0f;
     [SerializeField] private float mCameraAngleOverride = 0.0f;
-    [SerializeField] private float mThreshold = 0.01f;
-    [SerializeField] private float mGroundedTopClamp = 70.0f;
-    [SerializeField] private float mGroundedBottomClamp = -30.0f;
-    
-    // 공중 시야 제한 완화(90로도 하면 공격감지 콜라이더에 문제 생김)
-    private float mAirTopClamp = 89f;
-    private float mAirBottomClamp = -89f;
+    [SerializeField] private const float mThreshold = 0.01f;
     
     // cinemachine
     private float mCinemachineTargetYaw;
     private float mCinemachineTargetPitch;
-    private bool mbIsGround;
 
     private void Awake()
     {
@@ -45,11 +40,6 @@ public class CameraController : MonoBehaviour
             Cursor.lockState = Cursor.visible ? CursorLockMode.None : CursorLockMode.Locked;
         }
     }
-
-    public void SendPlayerGrounded(bool isGround)
-    {
-        mbIsGround = isGround;
-    }
     
     private void CameraRotation()
     {
@@ -60,15 +50,7 @@ public class CameraController : MonoBehaviour
         }
     
         mCinemachineTargetYaw = ClampAngle(mCinemachineTargetYaw, float.MinValue, float.MaxValue);
-
-        if (mbIsGround)
-        {
-            mCinemachineTargetPitch = ClampAngle(mCinemachineTargetPitch, mGroundedBottomClamp, mGroundedTopClamp);
-        }
-        else
-        {
-            mCinemachineTargetPitch = ClampAngle(mCinemachineTargetPitch, mAirBottomClamp, mAirTopClamp);
-        }
+        mCinemachineTargetPitch = ClampAngle(mCinemachineTargetPitch, mBottomClamp, mTopClamp);
         
         mCinemachineCameraTarget.transform.rotation = Quaternion.Euler(mCinemachineTargetPitch + mCameraAngleOverride,
             mCinemachineTargetYaw, 0.0f);
--- Assets/@02.Scripts/04.Player/PlayerController.cs
+++ Assets/@02.Scripts/04.Player/PlayerController.cs
@@ -11,24 +11,49 @@ using UnityEngine.Serialization;
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour, IObserver<GameObject>
 {
+    /*[Header("Player Basic Stat")]
+    [SerializeField] private int mMaxHealth = 100;
+    [SerializeField] private int mBaseAttackPower = 10;
+    [SerializeField] private int mBaseDefendPower = 5;
     
-    [Header("Control Variable")]
+    [Space(10)]
+    [Header("Player Current Changed Stat")]
+    [SerializeField] private int mCurrentHealth;
+    public int mCurrentAttackPower;
+    public int mCurrentDefendPower;
+    public float mSpeed;*/
+    
+    [Space(10)]
+    [Header("Player Move Stat")]
+    /*[SerializeField] private float mMoveSpeed = 4.0f;
+    [SerializeField] private float mSprintSpeed = 6.0f;*/
     [SerializeField] private float mRotationSmoothTime = 0.12f;
     [SerializeField] private float mSpeedChangeRate = 10.0f;
+    
+    [Space(10)]
+    [Header("Player Jump Stat")]
     [SerializeField] private float mGravity = - 9.81f;
-    [SerializeField] private float mJumpTimeout = 0.5f;
-    [SerializeField] private float mFallTimeout = 0.15f;
     [SerializeField] private float mJumpHeight = 5.0f;
+    [SerializeField] private float mJumpTimeout = 0.5f;
+    public float JumpTimeout => mJumpTimeout;
+    [SerializeField] private float mFallTimeout = 0.15f;
+    public float FallTimeout => mFallTimeout;
+    
+    [Space(10)]
+    [Header("Player Roll Stat")]
     [SerializeField] private float mRollDistance = 5.0f;
+    
+    [Space(10)]
+    [Header("Player Dash Stat")]
     [SerializeField] private float mDashDistance = 10.0f;
     
     [Space(10)]
     [Header("Player Grouned Check")]
-    [SerializeField] private LayerMask mGroundLayers;
-    [SerializeField] private float mGroundedOffset = -0.15f;
-    [SerializeField] private float mGroundedRadius = 0.3f;
     [SerializeField] private bool mbIsGrounded = true;
     public bool IsGrounded => mbIsGrounded;
+    [SerializeField] private float mGroundedOffset = -0.15f;
+    [SerializeField] private float mGroundedRadius = 0.3f;
+    [SerializeField] private LayerMask mGroundLayers;
     
     [Space(10)]
     [Header("Player Attach Point")]
@@ -39,6 +64,7 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
     [Header("Reference")]
     [SerializeField] private PlayerStats mPlayerStats;
     
+    
     // Player Calculation Stat
     [SerializeField]
     private float mVerticalVelocity;
@@ -46,15 +72,16 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
     private float mTerminalVelocity = 53.0f;
     private float mTargetRotation;
     private float mAnimationBlend;
-    private float mSpeed;
+
+    public float mSpeed;
     
     private bool mbInCombat = false;
-    private float mInCombatTimeout = 10.0f;
+    private float mInCombatTimeout = 5.0f;
     
     // Timeout Deltatime
     private float mJumpTimeoutDelta;
     private float mFallTimeoutDelta;
-    public float mInCombatTimeoutDelta;
+    private float mInCombatTimeoutDelta;
     
     
     // Componenet
@@ -172,6 +199,11 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
         
         SetPlayerState(PlayerState.Idle);
         
+        /*// 스탯 초기화
+        mCurrentAttackPower = mBaseAttackPower;
+        mCurrentDefendPower = mBaseDefendPower;
+        mCurrentHealth = mMaxHealth;*/
+        
         // 무기 할당
         SetPlayerWeapon(mRightHandTransform, "Longsword", mLeftHandTransform, "Shield");
     }
@@ -196,15 +228,13 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
             QueryTriggerInteraction.Ignore);
 
         PlayerAnimator.SetBool("IsGrounded", mbIsGrounded);
-        
-        mMainCamera.GetComponent<CameraController>().SendPlayerGrounded(mbIsGrounded);
     }
     
     private void ApplyGravity()
     {
         if (mbIsGrounded)
         {
-            mFallTimeoutDelta = mFallTimeout;
+            mFallTimeoutDelta = FallTimeout;
              
             if (mJumpTimeoutDelta >= 0.0f)
             {
@@ -218,7 +248,7 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
         }
         else
         {
-            mJumpTimeoutDelta = mJumpTimeout;
+            mJumpTimeoutDelta = JumpTimeout;
             
             if (mFallTimeoutDelta >= 0.0f)
             {
@@ -243,8 +273,8 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
     {
         if (mbInCombat)
         {
-            mInCombatTimeoutDelta -= Time.deltaTime;
-            if (mInCombatTimeoutDelta <= 0.0f)
+            mInCombatTimeout -= Time.deltaTime;
+            if (mInCombatTimeout <= 0.0f)
             {
                 mbInCombat = false;
             }
@@ -266,21 +296,10 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
         
         return cameraForward;
     }
-    
+
     public Vector3 SetTargetDirection()
     {
-        var targetDirection = GetCameraForwardDirection();
-        
-        if (GameManager.Instance.Input.MoveInput != Vector2.zero)
-        {
-            Vector3 inputDirection = 
-                new Vector3(GameManager.Instance.Input.MoveInput.x, 0.0f, GameManager.Instance.Input.MoveInput.y).normalized;
-            float targetRotation = 
-                Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + mMainCamera.transform.eulerAngles.y;
-
-            targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
-        }
-        
+        Vector3 targetDirection = Quaternion.Euler(0.0f, mTargetRotation, 0.0f) * Vector3.forward;
         return targetDirection;
     }
     
@@ -331,20 +350,14 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
         // 4. 방향 처리 (회전 허용 여부 분기)
         Vector3 inputDirection = 
             new Vector3(GameManager.Instance.Input.MoveInput.x, 0.0f, GameManager.Instance.Input.MoveInput.y).normalized;
+        
         mTargetRotation = 
             Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + mMainCamera.transform.eulerAngles.y;
         
         if (allowRotation)
         {
-            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, mTargetRotation, 
-                                                                        ref mRotationVelocity, mRotationSmoothTime);
-            Vector3 rotationDirection =new Vector3(0.0f, rotation, 0.0f);
-            transform.rotation = Quaternion.Euler(rotationDirection);
-        }
-        
-        if (mbInCombat)
-        {
-            transform.rotation = Quaternion.LookRotation(GetCameraForwardDirection());
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, mTargetRotation, ref mRotationVelocity, mRotationSmoothTime);
+            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
         }
         
         Vector3 targetDirection = Quaternion.Euler(0.0f, mTargetRotation, 0.0f) * Vector3.forward;
@@ -357,34 +370,10 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
         {
             mCharacterController.Move(targetDirection.normalized * (mSpeed * Time.deltaTime) + new Vector3(0.0f, mVerticalVelocity, 0.0f) * Time.deltaTime);
         }
-
-        if (mbInCombat)
-        {
-            Vector3 direction = GameManager.Instance.Input.MoveInput;
-            
-            // 현재 애니메이터 파라미터 값 가져오기
-            float currentVertical = mPlayerAnimator.GetFloat("Vertical");
-            float currentHorizontal = mPlayerAnimator.GetFloat("Horizontal");
-    
-            // 목표 값 설정
-            float targetVertical = direction.y;
-            float targetHorizontal = direction.x;
-            
-            // 부드러운 전환을 위한 보간
-            float smoothedVertical = Mathf.Lerp(currentVertical, targetVertical, Time.deltaTime * mSpeedChangeRate);
-            float smoothedHorizontal = Mathf.Lerp(currentHorizontal, targetHorizontal, Time.deltaTime * mSpeedChangeRate);
-            
-            
-            mPlayerAnimator.SetFloat("Vertical", smoothedVertical);
-            mPlayerAnimator.SetFloat("Horizontal", smoothedHorizontal);
-        }
-        else
-        {
-            mPlayerAnimator.SetFloat("Vertical", 1.0f);
-            mPlayerAnimator.SetFloat("Horizontal", 0.0f);
-        }
         
         mPlayerAnimator.SetFloat("Speed", mAnimationBlend);
+        mPlayerAnimator.SetFloat("Vertical", GameManager.Instance.Input.MoveInput.y); // 임시
+        mPlayerAnimator.SetFloat("Horizontal", GameManager.Instance.Input.MoveInput.x); //임시
         mPlayerAnimator.SetFloat("MotionSpeed", inputMagnitude);
     }
     
@@ -502,7 +491,7 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
     public void EnterCombat()
     {
         mbInCombat = true;
-        mInCombatTimeoutDelta = mInCombatTimeout;
+        mInCombatTimeout = mInCombatTimeoutDelta;
     }
 
     /*public void AddItemAttack(int itemAttackPower)
@@ -513,7 +502,6 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
     public void Attack()
     {
         GameManager.Instance.Input.SprintOff();
-        EnterCombat();
         
         // 이동 공격시 하반신(Base Layer) 애니메이션
         if (GameManager.Instance.Input.MoveInput == Vector2.zero)
@@ -543,7 +531,7 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
                         mVerticalVelocity = 0.0f;
                         mVerticalVelocity = Mathf.Sqrt(mJumpHeight * -2.0f * mGravity);
                         
-                        mPlayerAnimator.SetBool("Jump", true);
+                        mPlayerAnimator.SetBool("Jump", false);
                     }
                 }
             }
@@ -560,8 +548,7 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
     // 공격 애니메이션의 공격 모션 시작 시 호출 메서드
     public void MeleeAttackStart()
     {
-        if (CurrentPlayerState == PlayerState.Attack || CurrentPlayerState == PlayerState.Parry 
-                                                     || CurrentPlayerState == PlayerState.Dash)
+        if (CurrentPlayerState == PlayerState.Attack)
         {
             mWeaponController.AttackStart();
         }
@@ -570,8 +557,7 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
     // 공격 애니메이션의 공격 모션 종료 시 호출되는 메서드
     public void MeleeAttackEnd()
     {
-        if (CurrentPlayerState == PlayerState.Attack || CurrentPlayerState == PlayerState.Parry 
-                                                     || CurrentPlayerState == PlayerState.Dash)
+        if (CurrentPlayerState == PlayerState.Attack)
         {
             mWeaponController.AttackEnd();
         }
@@ -645,7 +631,12 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
     {
         mPlayerStats.OnGuardSuccess();
     }
-    
+
+    /*public void AddItemDefend(int itemDefendPower)
+    {
+        mCurrentDefendPower += itemDefendPower;
+    }*/
+
     #endregion
 
     #region 패리 관련 기능
@@ -653,7 +644,6 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
     public void Parry()
     {
         GameManager.Instance.Input.SprintOff();
-        EnterCombat();
 
         // 패리 성공 시 행동 메서드
         ParrySuccess();
@@ -725,8 +715,6 @@ public class PlayerController : MonoBehaviour, IObserver<GameObject>
         }
         else
         {
-            EnterCombat();     
-            
             SetPlayerState(PlayerState.Hit);
             // 방향에 따라 맞는 애니메이션이 없으므로 현재는 효과가 없는 것이나 마찬가지인 상태, 일단 대기
             // 플레이어 캐릭터의 방향을 회전시켜주는 수동적인 방식을 사용하거나?
