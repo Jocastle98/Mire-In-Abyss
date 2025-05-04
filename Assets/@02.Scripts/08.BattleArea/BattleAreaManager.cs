3024f8788be7ecf6c3031daf847704e4e1a58eed
puplejar <127178753+puplejar@users.noreply.github.com>
2025-05-04 15:18:50 +0900

    chore : 모든배틀지역 연결
    배틀매니저로부터 초기화
    필드,던전 생성 및 반복
    포탈, 모든 배틀구역 클리어시 반환
    을 연결

--- Assets/@02.Scripts/08.BattleArea/BattleAreaManager.cs
+++ Assets/@02.Scripts/08.BattleArea/BattleAreaManager.cs
@@ -1,112 +1,127 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 /// <summary>
 /// 필드와 던전을 관리하고 생성하는 매니저
 /// </summary>
 public class BattleAreaManager : MonoBehaviour
 {
-    
-    //매니저
-    public int battleAreaClearLimit = 5;
-    
-    private int mLevelDesign = 1;
-    private GameObject mPlayer;
-    private GameObject mCurrentArea;
-    private int mBattleAreaClearCount = 0;
-    
-    //필드
-    public GameObject[] battleFields;
-    
-    //던전
-    public int dungeonMinSize;
-    public int dungeonDivideWidth;
-    public int dungeonMinRoomCount;
-    
-    private GameObject mBattleDungeons;
-    private Vector2Int mDungeonMinSize;
 
-    private BattleArea TestArea;
+    [Header("매니저")] 
+    [SerializeField]private int mLevelDesign = 1;
+    [SerializeField]private int mBattleAreaClearLimit = 5;
+    [SerializeField]private int mBattleAreaClearCount = 0;
+    private GameObject mPortal;
     
-    //매개변수를 어떻게 받을지는 게임매니저에 따라 달라질 수 있음
-    public void BattleAreaManagerInit(GameObject player, int levelDesign)
+    [SerializeField]private GameObject mPlayer;
+    [SerializeField]private GameObject mCurrentArea;
+
+    [Space(10)] [Header("던전")]
+    [SerializeField]private int mCellSize = 3;
+    [SerializeField]private int mMinDungeonRoomSize = 10;
+    [SerializeField]private int mEventRoomChance = 10;
+    
+    [Space(10)]
+    [SerializeField][Range(0, 4)] private int mDivideLineWidth = 2;
+    [SerializeField][Range(2, 20)]private int mMinRoomCount = 6;
+    
+    [Space(10)] [Header("드래그 할당")] 
+    [Header("포탈")] [SerializeField]private GameObject mPortalPrefab;
+    [Header("필드")] [SerializeField]private List<FieldDataSO> mBattleFields;
+    [Header("던전")] [SerializeField]private SODungeonList mDungeonListSO;
+
+    private void Start()
+    {
+        BattleAreaManagerInit(mPlayer, 1, 5);
+    }
+
+    /// <summary>
+    /// 매니저 초기화 및 필드,던전을 생성하는 함수
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="levelDesign"></param>
+    /// <param name="battleAreaClearLimit"></param>
+    public void BattleAreaManagerInit(GameObject player, int levelDesign, int battleAreaClearLimit)
     {
         mPlayer = player;
         mLevelDesign = levelDesign;
+        mBattleAreaClearLimit = battleAreaClearLimit;
+        mPortal = Instantiate(mPortalPrefab);
 
-        //mBattleDungeons = new GameObject("BattleDungeonManager");
-        //mBattleDungeons.AddComponent<DungeonController>();
+        BattleAreaCreate();
         Debug.Log("Init succeed!");
     }
-    
+
     /// <summary>
     /// 필드 혹은 던전을 생성하는 함수
-    /// 처음 게임 씬이 로드될 때 한번만 실행함.
     /// </summary>
-    public void BattleAreaCreate()
+    private void BattleAreaCreate()
     {
-
         Debug.Log("Creating...");
-        if (mBattleAreaClearCount % 2 == 0)
+        if (mBattleAreaClearCount % 2 == 1)
         {
             Debug.Log("...Field");
-            mCurrentArea = Instantiate(battleFields[Random.Range(0, battleFields.Length)]);
+            //생성 및 초기화
+            FieldDataSO fieldData = mBattleFields[Random.Range(0, mBattleFields.Count)];
+            mCurrentArea = Instantiate(fieldData.battleFields);
             FieldController fc = mCurrentArea.GetComponent<FieldController>();
-            fc.BattleAreaInit(mPlayer, mLevelDesign);
-
+            fc.SetPortal(mPortal);
+            fc.FieldInit(mPlayer, mLevelDesign, fieldData);
+            
+            //클리어 델리게이트 부착
             fc.OnClearBattleArea -= BattleAreaClear;
             fc.OnClearBattleArea += BattleAreaClear;
-
-            TestArea = fc;
-            Debug.Log(mBattleAreaClearCount);
-
         }
         else
         {
             Debug.Log("...Dungeon");
-            // DungeonController dm = mBattleDungeons.GetComponent<DungeonController>();
-            // dm.BattleDungeonInit(mPlayer, mLevelDesign, mDungeonMinSize, dungeonMaxSize);
-            //
-            // dm.OnClearBattleArea -= BattleAreaClear;
-            // dm.OnClearBattleArea += BattleAreaClear;
-            Debug.Log("Dungeeeeeeeeeeeeeeeeeeeon"+ ++mBattleAreaClearCount);
+            //생성 및 초기화
+            GameObject dungeon = new GameObject("Dungeon");
+            DungeonController dCon = dungeon.AddComponent<DungeonController>();
+            dCon.SetPortal(mPortal);
+            dCon.DungeonInit(mCellSize, mMinDungeonRoomSize, mDivideLineWidth, mMinRoomCount, mLevelDesign,
+                     mEventRoomChance, mDungeonListSO, mPlayer);
+            
+            //클리어 델리게이트 부착
+            dCon.OnClearBattleArea -= BattleAreaClear;
+            dCon.OnClearBattleArea += BattleAreaClear;
         }
+
         Debug.Log("Create succeed!");
     }
 
     /// <summary>
     /// 필드 혹은 던전 클래스에서 클리어함수가 실행된 후 호출될 함수
     /// </summary>
-    public void BattleAreaClear()
+    private void BattleAreaClear()
     {
+        mLevelDesign++;
         mBattleAreaClearCount++;
         Debug.Log("Clear succeed! : " + mBattleAreaClearCount);
-        mLevelDesign++; // 필드 혹은 던전 마다 크기가 달라 몬스터,보물상자 스폰수를 다르게 적용해야하니 직접 컨트롤할 필요 없음
-        
-        if (mBattleAreaClearCount >= battleAreaClearLimit)
+
+        if (mBattleAreaClearCount >= mBattleAreaClearLimit)
         {
             LetsGoHome();
         }
-        
-        TestArea = null;
+        else
+        {
+            BattleAreaCreate();
+        }
+
     }
-    
+
     /// <summary>
     /// 집으로가는 함수
     /// </summary>
-    public void LetsGoHome()
+    private void LetsGoHome()
     {
         //마을 씬로드
         Debug.Log("LetsGoHome");
     }
-
-    public void TestBattleAreaClear()
-    {
-        TestArea?.BattleAreaClear();
-    }
-
 }
  
--- Assets/@02.Scripts/08.BattleArea/Dungeon/DungeonSO/SOMonsters.cs
+++ Assets/@02.Scripts/08.BattleArea/Dungeon/DungeonSO/SOMonsters.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "CreateSO/SOMonster", fileName = "New SOMonster")]
+[CreateAssetMenu(menuName = "BattleAreaSo/SOMonster", fileName = "New SOMonster")]
 public class SOMonsters : ScriptableObject
 {
     public List<GameObject> monsters = new List<GameObject>();
