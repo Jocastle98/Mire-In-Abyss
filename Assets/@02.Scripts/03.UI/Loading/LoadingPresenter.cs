bd68640c5aa6eefbc7c3ceb554585414b4ba5a9c
pianoop <kphjjh723@gmail.com>
2025-05-09 12:14:01 +0900

    feat: Boot Scene UI

---
+++ Assets/@02.Scripts/03.UI/Loading/LoadingPresenter.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public sealed class LoadingPresenter : MonoBehaviour
+{
+    [SerializeField] ProgressBarUI mPregressBar;
+    [SerializeField] TMP_Text mPercentTxt;
+
+    void Update()
+    {
+        /* 진행률 업데이트 */
+        // var p = SceneLoader.Progress;                  // 0~1
+        // mPregressBar.SetProgress(p);
+        // mPercentTxt.text = p * 100.ToString("F1") + "%";
+    }
+}
